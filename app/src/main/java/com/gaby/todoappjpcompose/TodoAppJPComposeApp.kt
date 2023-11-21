c50a8055acae5c7a3171e1e65fd835ba03229be9
GabbsM <gabbsmoreno.gm@gmail.com>
2023-11-22 00:17:44 +0100

    First commit:
    - Preparación de proyecto
    - Inyección de dependencias
    - Scren principal
    - Configuración de Dialog

---
+++ app/src/main/java/com/gaby/todoappjpcompose/TodoAppJPComposeApp.kt
@@ -0,0 +1,8 @@
+package com.gaby.todoappjpcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoAppJPComposeApp : Application() {
+}
---
+++ app/src/main/java/com/gaby/todoappjpcompose/addtask/ui/TasksViewModel.kt
@@ -0,0 +1,14 @@
+package com.gaby.todoappjpcompose.addtask.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TasksViewModel @Inject constructor():ViewModel() {
+
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog : LiveData<Boolean> = _showDialog
+}
